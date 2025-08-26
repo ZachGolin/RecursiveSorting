42fe9144a88558db69cc6e8dbdda5e377ab34a68
ZachGolin <zacharygolin2001@gmail.com>
2025-08-26 11:39:48 -0700

    Add project files. Also, reminder to allow MergeSort to accept a lambda defining the sorting order on type T

---
+++ RecursiveSorting/Sorts.cs
@@ -0,0 +1,63 @@
+﻿namespace RecursiveSorting
+{
+    public static class Sorts<T> where T : IComparable<T>
+    {
+        public static List<T> MergeSort(List<T> vals) //TODO: re-work this to allow for usage of a lambda to define a sort order
+        {
+            List<T> elements = [];
+            elements.AddRange(vals);
+            if (elements.Count <= 1) { return elements; }
+
+            //divide
+            List<T> firsts = new();
+            List<T> lasts = new();
+            int split = (elements.Count / 2);
+            for (int l = 0; l < elements.Count; l++)
+            {
+                if (l < split)
+                {
+                    firsts.Add(elements[l]);
+                }
+                else
+                { 
+                    lasts.Add(elements[l]);
+                }
+            }
+            
+            //recur
+            firsts = MergeSort(firsts);
+            lasts = MergeSort(lasts);
+
+
+            //incorporate
+            int i = 0;
+            int j = 0;
+            for (int k = 0; k < elements.Count; k++)
+            {
+                if (i >= firsts.Count)
+                {
+                    elements[k] = lasts[j];
+                    j++;
+                    continue;
+                }
+                else if (j >= lasts.Count)
+                {
+                    elements[k] = firsts[i];
+                    i++;
+                    continue;
+                }
+                if (firsts[i].CompareTo(lasts[j]) <= 0) //must be <= to ensure sort stability
+                {
+                    elements[k] = firsts[i];
+                    i++;
+                }
+                else
+                {
+                    elements[k] = lasts[j];
+                    j++;
+                }
+            }
+            return elements;
+        }
+    }
+}
