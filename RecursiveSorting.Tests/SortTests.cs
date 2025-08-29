0fff68dc2736c8bac481a208d1e0446967ed54b5
ZachGolin <zacharygolin2001@gmail.com>
2025-08-28 20:27:39 -0700

    fixed quicksort!

--- RecursiveSorting.Tests/SortTests.cs
+++ RecursiveSorting.Tests/SortTests.cs
@@ -1,4 +1,5 @@
 using System.Windows.Markup;
+using static RecursiveSorting.Tests.TestUtils;
 
 namespace RecursiveSorting.Tests
 {
@@ -9,16 +10,20 @@ namespace RecursiveSorting.Tests
             return string.Join(",", values);
         }
 
-        public class Mnemonic : IComparable<Mnemonic>
+        public class Mnemonic : IComparable<Mnemonic?> //a reference type to use as a test subject
         {
             public int Value { get; set; }
             public int Order { get; set; }
 
             public int CompareTo(Mnemonic? other)
             {
-                return Value.CompareTo(other?.Value);
+                if (other is null) { return -1; } //assume this mnemonic precedes a null mnemonic
+                return Value.CompareTo(other.Value);
             }
         }
+
+        public static Comparison<int> IntOrder = (a,b) => (a.CompareTo(b));
+        public static Comparison<Mnemonic?> MnemonicOrder = (a,b) => (a?.CompareTo(b)) ?? 1; //assume null mnemonics follow all others
     }
 
 
@@ -33,37 +38,108 @@ namespace RecursiveSorting.Tests
         [InlineData(new int[] { 11, 7, 5, 11, 2 })]
         [InlineData(new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 })]
         [InlineData(new int[] { -11, 7, -5, 3, -2 })]
-        public void MergeSort_Sorts_Properly<T>(T[] vals) where T : IComparable<T>
+        public void MergeSort_Sorts_Properly(int[] vals)
         {
-            List<T> list = vals.ToList();
-            List<T> sortedList = Sorts<T>.MergeSort(list);
-            list.Sort();
+            List<int> list = vals.ToList();
+            List<int> sortedList = Sorts<int>.MergeSort(list, IntOrder);
+            list.Sort(IntOrder);
             bool result = list.SequenceEqual(sortedList);
-            Assert.True(result, $"Sequences {TestUtils.ListToCommaSeparatedString(list)} and {TestUtils.ListToCommaSeparatedString(sortedList)} are not equal");
+            Assert.True(result, $"Sequences {ListToCommaSeparatedString(list)} and {ListToCommaSeparatedString(sortedList)} are not equal");
         }
 
         [Fact]
         public void MergeSort_IsNot_Destructive()
         {
+            //original list should remain unchanged
             List<int> list = [1, 2, 5, 4, 3];
             List<int> identicalList = [1, 2, 5, 4, 3];
-            List<int> sortedList = Sorts<int>.MergeSort(list);
-            Assert.True(list.SequenceEqual(identicalList), "MergeSort modified input list!");
+            List<int> sortedList = Sorts<int>.MergeSort(list, IntOrder);
+            Assert.True(list.SequenceEqual(identicalList), "MergeSort modified input list! Original list order was altered!");
+
+            //references to objects in both lists should also remain unchanged
+            List<Mnemonic?> mnemonics = [
+                new() { Value = 2, Order = 1 },
+                new() { Value = 3, Order = 2 },
+                new() { Value = 1, Order = 3 }
+            ];
+            List<Mnemonic?> sortedMnemonics = Sorts<Mnemonic?>.MergeSort(mnemonics, MnemonicOrder);
+            Assert.True(ReferenceEquals(mnemonics[0], sortedMnemonics[1]), "MergeSort modified input list! Element references were altered!");
         }
 
         [Fact]
         public void MergeSort_Is_Stable()
-        { 
-            List<TestUtils.Mnemonic> mnemonics = new();
-            mnemonics.Add(new() { Value = 2, Order = 1 });
-            mnemonics.Add(new() { Value = 2, Order = 2 });
-            mnemonics.Add(new() { Value = 2, Order = 3 });
-            mnemonics.Add(new() { Value = 2, Order = 4 });
-            List<TestUtils.Mnemonic> sortedMnemonics = Sorts<TestUtils.Mnemonic>.MergeSort(mnemonics);
+        {
+            List<Mnemonic?> mnemonics = [
+                new() { Value = 2, Order = 1 },
+                new() { Value = 2, Order = 2 },
+                new() { Value = 2, Order = 3 },
+                new() { Value = 2, Order = 4 }
+            ];
+            List<Mnemonic?> sortedMnemonics = Sorts<Mnemonic?>.MergeSort(mnemonics, MnemonicOrder);
             for (int i = 0; i < sortedMnemonics.Count - 1; i++)
             {
-                Assert.True(sortedMnemonics[i].Order < sortedMnemonics[i + 1].Order, "Instability in MergeSort detected!");
+                Assert.NotNull(sortedMnemonics[i]);
+                Assert.NotNull(sortedMnemonics[i + 1]);
+                Assert.True(sortedMnemonics[i]!.Order < sortedMnemonics[i + 1]!.Order, "Instability in MergeSort detected!");
             }
         }
+
+        [Fact]
+        public void MergeSort_PlacesNullReferences_Last() //this behavior only holds if the Order follows certain properties. the Mnemonic order *does* follow such properties. this test is not strictly necessary
+        {
+            List<Mnemonic?> mnemonics = [
+                new() { Value = 2, Order = 1 },
+                new() { Value = 2, Order = 2 },
+                null,
+                new() { Value = 2, Order = 4 }
+            ];
+            List<Mnemonic?> sortedMnemonics = Sorts<Mnemonic?>.MergeSort(mnemonics, MnemonicOrder);
+            mnemonics.Sort(MnemonicOrder);
+            Assert.True(mnemonics[3] is null, "Mnemonic ordering does not place null at the end of the sort order; please fix this delegate!");
+            Assert.True(sortedMnemonics[3] is null, "null reference was not placed at the end of the sort order!");
+        }
+
+
+
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 2 })]
+        [InlineData(new int[] { 2, 3, 5, 7, 11 })]
+        [InlineData(new int[] { 11, 7, 5, 3, 2 })]
+        [InlineData(new int[] { 11, 7, 5, 11, 2 })]
+        [InlineData(new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 })]
+        [InlineData(new int[] { -11, 7, -5, 3, -2 })]
+        public void QuickSort_Sorts_Properly(int[] vals)
+        {
+            List<int> list = vals.ToList();
+            List<int> listCopy = [];
+            listCopy.AddRange(list);
+            Sorts<int>.QuickSort(list, IntOrder);
+            listCopy.Sort(IntOrder);
+            bool result = list.SequenceEqual(listCopy);
+            Assert.True(result, $"Sequences {ListToCommaSeparatedString(list)} and {ListToCommaSeparatedString(listCopy)} are not equal");
+        }
+
+        /*
+        [Fact]
+        public void QuickSort_PlacesNullReferences_Last() //this behavior only holds if the Order follows certain properties. the Mnemonic order *does* follow such properties. this test is not strictly necessary
+        {
+            //quicksort keeps finding excuses to swap nulls with each other... maybe relax this property of the sort
+            List<Mnemonic?> mnemonics = [
+                null,
+                new() { Value = 2, Order = 1 },
+                new() { Value = 2, Order = 2 },
+                new() { Value = 2, Order = 4 }
+            ];
+            List<Mnemonic?> mnemonicsCopy = [];
+            mnemonicsCopy.AddRange(mnemonics);
+            Sorts<Mnemonic?>.QuickSort(mnemonics, MnemonicOrder);
+            mnemonicsCopy.Sort(MnemonicOrder);
+            Assert.True(mnemonicsCopy[3] is null, "Mnemonic ordering does not place null at the end of the sort order; please fix this delegate!");
+            Assert.True(mnemonics[3] is null, "null reference was not placed at the end of the sort order!");
+        }
+        */
+
     }
 }
--- RecursiveSorting/Sorts.cs
+++ RecursiveSorting/Sorts.cs
@@ -1,16 +1,18 @@
-﻿namespace RecursiveSorting
+﻿using System.Windows.Markup;
+
+namespace RecursiveSorting
 {
-    public static class Sorts<T> where T : IComparable<T>
+    public static class Sorts<T>
     {
-        public static List<T> MergeSort(List<T> vals) //TODO: re-work this to allow for usage of a lambda to define a sort order
+        public static List<T?> MergeSort(List<T?> vals, Comparison<T?> Order)
         {
-            List<T> elements = [];
-            elements.AddRange(vals);
-            if (elements.Count <= 1) { return elements; }
+            List<T?> elements = [];
+            elements.AddRange(vals); //doing this to avoid referencing input list - causes the sort to be destructive otherwise
+            if (elements.Count <= 1) { return elements; } //in this case, elements are already sorted as there is nothing else to sort with
 
             //divide
-            List<T> firsts = new();
-            List<T> lasts = new();
+            List<T?> firsts = new();
+            List<T?> lasts = new();
             int split = (elements.Count / 2);
             for (int l = 0; l < elements.Count; l++)
             {
@@ -24,12 +26,12 @@
                 }
             }
             
-            //recur
-            firsts = MergeSort(firsts);
-            lasts = MergeSort(lasts);
+            //recur - assume firsts and lasts are now ordered
+            firsts = MergeSort(firsts, Order);
+            lasts = MergeSort(lasts, Order);
 
 
-            //incorporate
+            //incorporate - interleave lists, maintaining sort order
             int i = 0;
             int j = 0;
             for (int k = 0; k < elements.Count; k++)
@@ -46,7 +48,7 @@
                     i++;
                     continue;
                 }
-                if (firsts[i].CompareTo(lasts[j]) <= 0) //must be <= to ensure sort stability
+                if (Order(firsts[i], lasts[j]) <= 0) //must be <= to maintain stability
                 {
                     elements[k] = firsts[i];
                     i++;
@@ -57,7 +59,58 @@
                     j++;
                 }
             }
-            return elements;
+            return elements; //list is now ordered - return
+        }
+
+
+
+        public static bool QuickSort(List<T?> vals, Comparison<T?> Order) //convention: returns whether or not any sorting occurred this call
+        {
+            if (vals.Count <= 1) { return false; }
+            return QuickSort(vals, Order, 0, vals.Count - 1);
+        }
+
+        private static bool QuickSort(List<T?> vals, Comparison<T?> Order, int left, int right)
+        {
+            if (left >= right) { return false; }
+            T? pivot = vals[left];
+            int L = left - 1;
+            int R = right + 1;
+
+            //only add if absolutely necessary
+            //int i = left + 1;
+            //while (pivot is null && i < right)
+            //{
+            //    pivot = vals[i];
+            //    i++;
+            //}
+
+            while (true)
+            {
+                do
+                {
+                    L++;
+                } while (Order(vals[L], pivot) < 0);
+
+                do
+                {
+                    R--;
+                } while (Order(vals[R], pivot) > 0);
+
+                if (L >= R)
+                { 
+                    break;
+                }
+
+                T? temp = vals[L];
+                vals[L] = vals[R];
+                vals[R] = temp;
+            }
+
+            QuickSort(vals, Order, left, R); //sort preceding section
+            QuickSort(vals, Order, R + 1, right); //sort following section
+
+            return true;
         }
     }
 }
